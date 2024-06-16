f47e3b18df4a7322f7c14ce44bac5e688e75534f
MeAGwen <m3agwen@gmail.com>
2024-06-16 18:49:22 -0400

    feat: color

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const CONFIG = {
 
 	// Weather
 	weatherKey: 'f3b93d2989d96725df0e74d799cc26fb', // Write here your API Key
-	weatherIcons: 'Onedark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherIcons: 'White', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'F', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
