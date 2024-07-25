c92c57a5c5aeec0eeb7205a2018a3118a1dba59b
MeAGwen <m3agwen@gmail.com>
2024-07-24 22:58:45 -0400

    feat: list

--- config.js
+++ config.js
@@ -147,8 +147,8 @@ const CONFIG = {
 					link: 'https://tcbscans-manga.com/',
 				},
 				{
-					name: 'Among Us',
-					link: 'https://substackcdn.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F88d26018-fa1a-4b92-a8b9-d8ed3f9e178e_3840x2160.png',
+					name: 'Bitwarden',
+					link: 'https://vault.bitwarden.com/#/vault',
 				},
 				{
 					name: 'One Piece',
